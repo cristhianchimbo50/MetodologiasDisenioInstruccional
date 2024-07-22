0b086f554865ed25b1f02b2280843933ccae0243
cristhianchimbo50 <cristhian.chimbo@espoch.edu.ec>
2024-07-21 19:13:44 -0500

    casi acabo mi parte

--- js/nuevasFunc.js
+++ js/nuevasFunc.js
@@ -1,52 +1,52 @@
 function openmodGagne() {
-    var url = 'https://view.genially.com/669c5cd39c64fa0477cad8fa';
-    var width = 1200;
-    var height = 675;
+    var url = 'https://view.genially.com/669d8ab6079a5f2933e36e4a';
+    var width =1500;
+    var height = 1500;
     var left = (window.innerWidth / 2) - (width / 2);
     var top = (window.innerHeight / 2) - (height / 2);
     window.open(url, 'popup', 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left + ',scrollbars=yes');
 }
 
 function openmodBrig() {
-    var url = 'https://view.genially.com/669c5cd39c64fa0477cad8fa';
-    var width = 1200;
-    var height = 675;
+    var url = 'https://view.genially.com/669d96a5a0c4cb116daa8a4f';
+    var width =1500;
+    var height = 1500;
     var left = (window.innerWidth / 2) - (width / 2);
     var top = (window.innerHeight / 2) - (height / 2);
     window.open(url, 'popup', 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left + ',scrollbars=yes');
 }
 
 function openmodAssur() {
-    var url = 'https://view.genially.com/669c5cd39c64fa0477cad8fa';
-    var width = 1200;
-    var height = 675;
+    var url = 'https://view.genially.com/669d9844a0c4cb116dabe1e8';
+    var width =1500;
+    var height = 1500;
     var left = (window.innerWidth / 2) - (width / 2);
     var top = (window.innerHeight / 2) - (height / 2);
     window.open(url, 'popup', 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left + ',scrollbars=yes');
 }
 
 function openmodJona() {
-    var url = 'https://view.genially.com/669c5cd39c64fa0477cad8fa';
-    var width = 1200;
-    var height = 675;
+    var url = 'https://view.genially.com/669d9d339c64fa0477610718';
+    var width =1500;
+    var height = 1500;
     var left = (window.innerWidth / 2) - (width / 2);
     var top = (window.innerHeight / 2) - (height / 2);
     window.open(url, 'popup', 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left + ',scrollbars=yes');
 }
 
 function openmodDerickCarey() {
-    var url = 'https://view.genially.com/669c5cd39c64fa0477cad8fa';
-    var width = 1200;
-    var height = 675;
+    var url = 'https://view.genially.com/669d9a3ba0c4cb116dad6e4e';
+    var width =1500;
+    var height = 1500;
     var left = (window.innerWidth / 2) - (width / 2);
     var top = (window.innerHeight / 2) - (height / 2);
     window.open(url, 'popup', 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left + ',scrollbars=yes');
 }
 
 function openmodAddie() {
-    var url = 'https://view.genially.com/669c5cd39c64fa0477cad8fa';
-    var width = 1200;
-    var height = 675;
+    var url = 'https://view.genially.com/669d9f219c64fa0477631109';
+    var width =1500;
+    var height = 1500;
     var left = (window.innerWidth / 2) - (width / 2);
     var top = (window.innerHeight / 2) - (height / 2);
     window.open(url, 'popup', 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left + ',scrollbars=yes');
